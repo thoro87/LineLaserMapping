4675c1cb7e81ef79f42d1b9da42a8c62b6e77cc2
Thoro87 <thorondors@gmx.de>
2017-01-29 12:37:00 +0100

    Initial Setup
    - added libraries and references
    - basic webcam-setup working

---
+++ LineLaserMapping/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace LineLaserMapping {
+	partial class MainForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			this.cameraControl = new Camera_NET.CameraControl();
+			this.comboBoxCameraList = new System.Windows.Forms.ComboBox();
+			this.comboBoxResolutionList = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// cameraControl
+			// 
+			this.cameraControl.DirectShowLogFilepath = "";
+			this.cameraControl.Location = new System.Drawing.Point(12, 12);
+			this.cameraControl.Name = "cameraControl";
+			this.cameraControl.Size = new System.Drawing.Size(297, 243);
+			this.cameraControl.TabIndex = 0;
+			// 
+			// comboBoxCameraList
+			// 
+			this.comboBoxCameraList.FormattingEnabled = true;
+			this.comboBoxCameraList.Location = new System.Drawing.Point(81, 338);
+			this.comboBoxCameraList.Name = "comboBoxCameraList";
+			this.comboBoxCameraList.Size = new System.Drawing.Size(268, 21);
+			this.comboBoxCameraList.TabIndex = 1;
+			this.comboBoxCameraList.SelectedIndexChanged += new System.EventHandler(this.comboBoxCameraList_SelectedIndexChanged);
+			// 
+			// comboBoxResolutionList
+			// 
+			this.comboBoxResolutionList.FormattingEnabled = true;
+			this.comboBoxResolutionList.Location = new System.Drawing.Point(355, 338);
+			this.comboBoxResolutionList.Name = "comboBoxResolutionList";
+			this.comboBoxResolutionList.Size = new System.Drawing.Size(125, 21);
+			this.comboBoxResolutionList.TabIndex = 2;
+			this.comboBoxResolutionList.SelectedIndexChanged += new System.EventHandler(this.comboBoxResolutionList_SelectedIndexChanged);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1019, 493);
+			this.Controls.Add(this.comboBoxResolutionList);
+			this.Controls.Add(this.comboBoxCameraList);
+			this.Controls.Add(this.cameraControl);
+			this.Name = "MainForm";
+			this.Text = "LineLaserMapping";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+			this.ResumeLayout(false);
+		}
+
+		#endregion
+
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+		private Camera_NET.CameraControl cameraControl;
+		private System.Windows.Forms.ComboBox comboBoxCameraList;
+		private System.Windows.Forms.ComboBox comboBoxResolutionList;
+	}
+}
+
---
+++ LineLaserMapping/MainForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Camera_NET;
+using DirectShowLib;
+
+namespace LineLaserMapping {
+	public partial class MainForm : Form {
+
+		private CameraChoice cameraChoice = new CameraChoice();
+
+		public MainForm() {
+			InitializeComponent();
+			FillCameraList();
+			FillResolutionList();
+
+
+		}
+
+		#region Camera_Internal
+		private void MainForm_FormClosed(object sender, FormClosedEventArgs e) {
+			cameraControl.CloseCamera();
+		}
+
+		private void comboBoxCameraList_SelectedIndexChanged(object sender, EventArgs e) {
+			if (comboBoxCameraList.SelectedIndex < 0) {
+				cameraControl.CloseCamera();
+			} else {
+				cameraControl.SetCamera(cameraChoice.Devices[comboBoxCameraList.SelectedIndex].Mon, null);
+			}
+			FillResolutionList();
+		}
+
+		private void comboBoxResolutionList_SelectedIndexChanged(object sender, EventArgs e) {
+			if (!cameraControl.CameraCreated) {
+				return;
+			}
+
+			int comboBoxResolutionIndex = comboBoxResolutionList.SelectedIndex;
+			if (comboBoxResolutionIndex < 0) {
+				return;
+			}
+			ResolutionList resolutions = Camera.GetResolutionList(cameraControl.Moniker);
+
+			if (resolutions == null || comboBoxResolutionIndex >= resolutions.Count() || resolutions[comboBoxResolutionIndex].CompareTo(cameraControl.Resolution) == 0) {
+				return;
+			}
+			cameraControl.SetCamera(cameraControl.Moniker, resolutions[comboBoxResolutionIndex]);
+		}
+
+		private void FillCameraList() {
+			comboBoxCameraList.Items.Clear();
+			cameraChoice.UpdateDeviceList();
+			foreach (var camera_device in cameraChoice.Devices) {
+				comboBoxCameraList.Items.Add(camera_device.Name);
+			}
+			if (comboBoxCameraList.Items.Count > 0) {
+				comboBoxCameraList.SelectedIndex = 0;
+			}
+		}
+
+		private void FillResolutionList() {
+			comboBoxResolutionList.Items.Clear();
+
+			if (!cameraControl.CameraCreated) {
+				return;
+			}
+
+			ResolutionList resolutions = Camera.GetResolutionList(cameraControl.Moniker);
+
+			if (resolutions == null) {
+				return;
+			}
+
+			cameraControl.SetCamera(cameraControl.Moniker, resolutions[5]);
+			int index_to_select = -1;
+
+			for (int index = 0; index < resolutions.Count; index++) {
+				comboBoxResolutionList.Items.Add(resolutions[index].ToString());
+
+				if (resolutions[index].CompareTo(cameraControl.Resolution) == 0) {
+					index_to_select = index;
+				}
+			}
+
+			if (index_to_select >= 0) {
+				comboBoxResolutionList.SelectedIndex = index_to_select;
+			}
+		}
+		#endregion
+
+
+	}
+}
